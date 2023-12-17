4e30b23b5b2c4ca4026a68f09170588ec624b932
NusretTinel <nusrettinel@outlook.com>
2023-12-18 01:37:12 +0300

    Yapı tamamlandı.Footer başlandı

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
   <HelloWorld></HelloWorld>
   </div>
 
@@ -23,7 +23,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 0px;
 }
 #app {
   font-family: Roboto, Helvetica, Arial, sans-serif;
--- src/components/Pages/Hizmetlerimiz/content.vue
+++ src/components/Pages/Hizmetlerimiz/content.vue
@@ -5,6 +5,6 @@
 </template>
 <script>
 export default{
-  name:'footerPage'
+  name:'hizmetlerimizPage'
 }
 </script>
